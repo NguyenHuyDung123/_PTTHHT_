de7dd75ab8cbbfd51c6cacf63c0207ae5d0cd7ee
NguyenHuyDung123 <90315395+NguyenHuyDung123@users.noreply.github.com>
2021-11-03 16:55:46 +0700

    baitap

---
+++ tuan8/client.java
@@ -0,0 +1,29 @@
+package tuan8;
+
+public class client {
+    package com.sept.client.impl;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import com.rmi.IAccount;
+
+public class RMIClient {
+    public static void main(String args[]) {
+        try {
+            //Xác định RMI máy chủ.
+            IAccount iAccount = (IAccount) Naming.lookup("rmi://192.168.1.230:6789/SeptemberRMI");
+            System.out.println("Name: " + iAccount.getUser().getUname());
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+}
+    
+}
